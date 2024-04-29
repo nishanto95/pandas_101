c80fec25ae24e240123d5aa7a98c9d8a252de824
nishanto95 <167629690+nishanto95@users.noreply.github.com>
2024-04-29 17:58:36 +0530

    Add files via upload

---
+++ P_column_operation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 23:25:14 2024
+
+@author: Nishant
+"""
+"""
+
+Which of these would be the correct way to create a dataframe?
+
+a.
+
+df = pd.DataFrame([[1, 2], ["Ram", "Shyam"], ["IT", "Ops"]], columns = ["emp_id", "name", "dept"])
+b.
+
+df = pd.DataFrame([[1, "Ram", "IT"], [2, "Shyam", "Ops"]], columns = ["emp_id", "name", "dept"])
+c.
+
+df = pd.DataFrame([1, "Ram", "IT"], columns = ["emp_id", "name", "dept"])
+d.
+
+df = pd.DataFrame({'emp_id':[1, 2], 'name': ['Ram', 'Shyam'], 'dept':['IT', 'Ops']})
+"""
+solution
+b and d
